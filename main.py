a909b0a8041c90c71ba19b8492741124092bd932
김채연 <100480440+chaerish@users.noreply.github.com>
2024-08-10 17:23:29 +0900

    feat: fast api 추가

---
+++ main.py
@@ -0,0 +1,119 @@
+from fastapi import FastAPI, File, UploadFile, Form
+import uvicorn
+import subprocess
+import json
+import os
+import requests
+import shutil
+from pydantic import BaseModel
+import os
+from pathlib import Path
+
+app = FastAPI()
+
+class UserSize(BaseModel):
+    chestSize: float
+    hipSize: float
+    shoulderSize: float
+    waistSize: float
+
+class UserBodySizeRequestDto(BaseModel):
+    userId: int
+    userSize: UserSize
+#루트경로
+BASE_DIR = Path(__file__).resolve().parent
+parent_dir = BASE_DIR.parent
+#이미지 저장 경로
+images_dir = BASE_DIR / "data/images"
+keypoints_dir = BASE_DIR / "data/keypoints"
+smplify_results_dir = BASE_DIR / "data/smplify_results"
+# 가상환경 경로 설정
+envs = {
+    "openpose": "openpose",
+    "smplifyx": "smplifyx",
+    "obj2json": "smpla"
+}
+
+def run_command(command, env_name):
+    #shell_script = "/Users/vecherish/Desktop/gradsmu/smplifyx/scripts/run_in_script.sh"  # 셸 스크립트 경로
+    shell_script = parent_dir / "scripts/run_in_script.sh"
+    full_command = f"bash {shell_script} {env_name} {command}"
+    process = subprocess.Popen(full_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout, stderr
+
+def delete_all_subfolders(base_dir):
+    for folder in ["images", "meshes", "results","meshes_png"]:
+        folder_path = os.path.join(base_dir, folder)
+        if os.path.exists(folder_path):
+            for subfolder in os.listdir(folder_path):
+                subfolder_path = os.path.join(folder_path, subfolder)
+                if os.path.isdir(subfolder_path):
+                    shutil.rmtree(subfolder_path)
+
+@app.post("/process")
+async def process_image(userId: int = Form(...), file: UploadFile = File(...)):
+    #파일 저장
+    #file_location = f"/Users/vecherish/Desktop/gradsmu/smplifyx/content/data/images/{file.filename}"
+    file_location = images_dir / file.filename
+    with open(file_location, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    # 1. OpenPose 실행
+    openpose_command = f"python scripts/openpose_script.py"
+    openpose_stdout, openpose_stderr = run_command(openpose_command, envs["openpose"])
+    
+    # print("OpenPose stdout:", openpose_stdout.decode())
+    # print("OpenPose stderr:", openpose_stderr.decode())
+    #키포인트 파일 경로
+    #keypoints_file = f"/Users/vecherish/Desktop/gradsmu/smplifyx/content/data/keypoints/{file.filename.replace('.jpg','')}_keypoints.json"
+    keypoints_file = keypoints_dir / f"{file.filename.replace('.jpg','')}_keypoints.json"
+    # 2. SMPLify 실행
+    smplify_command = f"python scripts/smplify_script.py"
+    smplify_stdout, smplify_stderr = run_command(smplify_command, envs["smplifyx"])
+   
+    #결과 obj 파일 경로
+    #obj_file = f"/Users/vecherish/Desktop/gradsmu/smplifyx/content/data/smplify_results/meshes/{file.filename.replace('.jpg', '')}/000.obj"
+    #obj_dir = f"/Users/vecherish/Desktop/gradsmu/smplifyx/content/data/smplify_results/meshes/{file.filename.replace('.jpg', '')}"
+    # 결과 obj 파일 경로
+    obj_file = smplify_results_dir / "meshes" / file.filename.replace('.jpg', '') / "000.obj"
+    obj_dir = smplify_results_dir / "meshes" / file.filename.replace('.jpg', '')
+    
+    # 3. OBJ to JSON 변환
+    obj2json_command = f"python scripts/obj2json_script.py --obj {obj_file}"
+    obj2json_stdout, obj2json_stderr = run_command(obj2json_command, envs["obj2json"])
+    # stdout으로부터 JSON 데이터를 파싱
+    obj2json_output = obj2json_stdout.decode()
+
+    json_data = json.loads(obj2json_output)
+
+    # 후에 모델로 변환
+    user_size = UserSize(**json_data)
+    user_body_size_request_dto = UserBodySizeRequestDto(userId=userId, userSize=user_size)
+    
+    response_data = user_body_size_request_dto.json()
+    print(response_data)
+    #obj.png file
+    obj_png= "/Users/vecherish/Desktop/gradsmu/smplifyx/content/data/smplify_results/meshes_png/000.png"
+    #obj_png = smplify_results_dir / "meshes_png" / "000.png"
+    multipart_data = {
+        "json":(None,response_data,"application/json"),
+        "file":("obj.png",obj_png,"image/png")
+    }
+    #전송
+    multipart_response=requests.post(
+        'http://localhost:8080/get/json', #수정
+        files = multipart_data
+    )
+
+    #원본 이미지와 임시 파일 삭제
+    os.remove(file_location)
+    os.remove(keypoints_file)
+    delete_all_subfolders(smplify_results_dir)
+    
+    if multipart_response.status_code == 200:
+        return {"status_code": multipart_response.status_code, "message": "Data sent successfully"}
+    else:
+        return {"status_code": multipart_response.status_code, "message": "Failed to send data"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
