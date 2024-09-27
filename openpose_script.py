af797c7567e56682c1e682f4d635681f8e3349b7
김채연 <100480440+chaerish@users.noreply.github.com>
2024-09-27 18:13:41 +0900

    fix: Update openpose_script.py ver gpu

--- openpose_script.py
+++ openpose_script.py
@@ -4,17 +4,20 @@ import argparse
 def main():
     # 오픈포즈 명령어 실행
     command = (
-        f"./build/examples/openpose/openpose.bin --image_dir /Users/vecherish/Desktop/gradsmu/smplifyx/content/data/images "
-        f"--write_json /Users/vecherish/Desktop/gradsmu/smplifyx/content/data/keypoints "
+        f"./examples/openpose/openpose.bin --image_dir /root/smplifyx/content/data/images "
+        f"--write_json /root/smplifyx/content/data/keypoints "
         f"--face --hand --display 0 "
-        f"--render_pose 0"
+        f"--render_pose 0 "
+        f"--model_pose BODY_25 "
+        f"--net_resolution 320x176 "
+        f"--face_net_resolution 320x320 "
         #f"--write_images /Users/vecherish/Desktop/gradsmu/smplifyx/content/openpose_images"
     )
-    working_directory = "/Users/vecherish/Desktop/gradsmu/smplifyx/content/openpose-with-caffe-for-MacM1"
+    working_directory = "/root/smplifyx/content/openpose"
     process = subprocess.Popen(command, shell=True, cwd=working_directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    # print("OpenPose stdout:", stdout.decode())
-    # print("OpenPose stderr:", stderr.decode())
+    print("OpenPose stdout:", stdout.decode())
+    print("OpenPose stderr:", stderr.decode())
 
 
 if __name__ == "__main__":
